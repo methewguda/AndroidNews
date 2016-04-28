a050327b00b60606f51173526a9bf9fd8212b39f
methewguda <methewguda@gmail.com>
2016-04-28 11:41:01 -0300

    Added Angular Moment

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-var app = angular.module('androidnews', ['ionic']);
+var app = angular.module('androidnews', ['ionic', 'angularMoment']);
 
 app.controller('AndroidNewsCtrl', function($http, $scope){
   
@@ -15,13 +15,7 @@ app.controller('AndroidNewsCtrl', function($http, $scope){
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
